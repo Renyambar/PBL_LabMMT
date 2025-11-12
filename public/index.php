073b501ae740a2a0e39ab1f0bcb6f51c7d59da3b
m1Febriansyah <muhammadfebriansyah1000@gmail.com>
2025-11-12 10:22:26 +0700

    login and index

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+// Load configuration
+require_once '../config/config.php';
+
+// Load core classes
+require_once '../app/core/Database.php';
+require_once '../app/core/Controller.php';
+require_once '../app/core/App.php';
+
+// Initialize app
+$app = new App();
